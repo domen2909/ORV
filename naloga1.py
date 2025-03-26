95ed0b61069c5d9c1b08e11fa6346fd9b15edeb2
domen2909 <domen.dsuban@student.um.si>
2025-03-26 21:12:46 +0100

    Dodana funkcija zmanjsaj_sliko in obdelaj sliko s skatlami

--- naloga1.py
+++ naloga1.py
@@ -2,17 +2,25 @@ import cv2 as cv
 import numpy as np
 
 def zmanjsaj_sliko(slika, sirina, visina):
-    '''Zmanjšaj sliko na velikost sirina x visina.'''
-    pass
+    # Zmanjšaj sliko na določeno velikost (sirina x visina) z interpolacijo cv.INTER_AREA
+    return cv.resize(slika, (sirina, visina), interpolation=cv.INTER_AREA)
 
 def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
-    '''Sprehodi se skozi sliko v velikosti škatle (sirina_skatle x visina_skatle) in izračunaj število pikslov kože v vsaki škatli.
-    Škatle se ne smejo prekrivati!
-    Vrne seznam škatel, s številom pikslov kože.
-    Primer: Če je v sliki 25 škatel, kjer je v vsaki vrstici 5 škatel, naj bo seznam oblike
-      [[1,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1]]. 
-      V tem primeru je v prvi škatli 1 piksel kože, v drugi 0, v tretji 0, v četrti 1 in v peti 1.'''
-    pass
+
+    visina, sirina, _ = slika.shape  # Dobimo dimenzije slike
+    skatle = []  # Seznam škatel v katerem bo število pikslov kože v skatli
+
+    # Sprehpdomo se čez sliko v korakih velikosti škatle
+    for y in range(0, visina, visina_skatle):
+        vrstica = []  
+        for x in range(0, sirina, sirina_skatle): # Sprehodimo se čez vrstico v korakih velikosti škatle
+            # Izrezeamo skatlo iz slike (x, y) -> (x + sirina_skatle, y + visina_skatle)
+            podslika = slika[y:y+visina_skatle, x:x+sirina_skatle]
+            stevilo_pikslov = prestej_piklse_z_barvo_koze(podslika, barva_koze)
+            vrstica.append(stevilo_pikslov)
+        skatle.append(vrstica)
+
+    return skatle
 
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
     '''Prestej število pikslov z barvo kože v škatli.'''
